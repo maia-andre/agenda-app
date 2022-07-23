e4fb05f6cb4ded619184152d49afafbda01d3f52
Andre Maia <andrefnkmm@gmail.com>
2022-07-23 19:20:52 -0300

    Criação do CardView para agendamentos

--- app/src/main/java/com/seduc/agendanews/MainActivity.java
+++ app/src/main/java/com/seduc/agendanews/MainActivity.java
@@ -2,8 +2,6 @@ package com.seduc.agendanews;
 
 import android.os.Bundle;
 
-import com.google.android.material.bottomnavigation.BottomNavigationView;
-
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.navigation.NavController;
 import androidx.navigation.Navigation;
@@ -24,7 +22,7 @@ public class MainActivity extends AppCompatActivity {
         setContentView(binding.getRoot());
 
         AppBarConfiguration appBarConfiguration = new AppBarConfiguration.Builder(
-                R.id.navigation_home, R.id.navigation_dashboard, R.id.navigation_notifications)
+                R.id.navigation_agendar, R.id.navigation_agendamento, R.id.navigation_usuario)
                 .build();
         NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment_activity_main);
         NavigationUI.setupActionBarWithNavController(this, navController, appBarConfiguration);
